649ac1d3c99744d67be16ea4a3c48963988b2e87
LucianThorr <jason.aylward@gmail.com>
2014-09-12 15:44:28 -0400

    Initial Commit

---
+++ DeltaMap/MapAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MapAppDelegate.h
+//  DeltaMap
+//
+//  Created by Jason Aylward on 9/12/14.
+//  Copyright (c) 2014 Jason Aylward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeltaMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeltaMap
+//
+//  Created by Jason Aylward on 9/12/14.
+//  Copyright (c) 2014 Jason Aylward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MapAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MapAppDelegate class]));
+    }
+}
